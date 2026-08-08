f25cef311321ca710accf0e2d7af3ea230841a4b
Michael <kontakt@michael-fiebelkorn.de>
2026-08-09 01:36:02 +0200

    template folder created and template written

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
 
@@ -5,7 +6,8 @@ from .models import Item
 def index(request):
   item_list = Item.objects.all() # alle Items fetchen
   # return HttpResponse("Hello World!")
-  return HttpResponse(item_list) # returned die Items-Namen
+  # return HttpResponse(item_list) # returned die Item-Namen
+  return render(request,"main_app/index.html") # schaut im Ordner templates/main_app nach dem HTML-Template
 
 
 def item(request):
