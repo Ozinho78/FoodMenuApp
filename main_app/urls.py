a12b88a25e5cfbdd40ceda4f8b9cb0a51d9168ba
Michael <kontakt@michael-fiebelkorn.de>
2026-08-09 03:12:58 +0200

    namespacing added in template

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,11 +2,13 @@
 
 from main_app import views
 
+app_name = 'main_app'
+
 # from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     # path('hello/', views.index),
     path('item/', views.item),
-    path('<int:id>/', views.detail),
+    path('<int:id>/', views.detail, name='detail'),
 ]
