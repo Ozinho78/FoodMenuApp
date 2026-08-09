ee3488f6da7a6b4c6380fc8a507374be1876c7ee
Michael <kontakt@michael-fiebelkorn.de>
2026-08-09 02:59:38 +0200

    detail view created and tested

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,12 @@
 ﻿from django.urls import path
+
 from main_app import views
+
 # from . import views
 
 urlpatterns = [
     path('', views.index),
     # path('hello/', views.index),
     path('item/', views.item),
+    path('<int:id>/', views.detail),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 from .models import Item
 
 
 def index(request):
-  item_list = Item.objects.all() # alle Items fetchen
-  # return HttpResponse("Hello World!")
-  # return HttpResponse(item_list) # returned die Item-Namen
-  return render(request,"main_app/index.html") # schaut im Ordner templates/main_app nach dem HTML-Template
+    # Getting items from the database
+    item_list = Item.objects.all()  # alle Items fetchen
+    # Creating context
+    context = {
+        # notwendig, damit Variable im HTML erkannt und gerendert wird
+        'item_list': item_list
+        # <QuerySet [<Item: Pizza>, <Item: Burger>, <Item: Burrito>, <Item: Cheesy Burger>]>
+    }
+    # return HttpResponse("Hello World!")
+    # return HttpResponse(item_list) # returned die Item-Namen
+    # schaut im Ordner templates/main_app nach dem HTML-Template
+    # Passing the context object to the render method alogn with the template
+    return render(request, "main_app/index.html", context)
+
+
+def detail(request, id):
+    # holt nur ein Item, dass der Id entspricht aus der DB
+    item = Item.objects.get(id=id)
+    context = {
+        'item': item
+    }
+    # return HttpResponse(f"This is the detail view for item with the id as {item}")
+    return render(request, "main_app/detail.html", context)
 
 
 def item(request):
-  return HttpResponse("This is an item view.")
+    return HttpResponse("This is an item view.")
