413b3ba554fa2a552d40852d48401cab9f0c6612
Michael <kontakt@michael-fiebelkorn.de>
2026-08-11 02:41:23 +0200

    base.html added and design improved

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Item
 
-# Register your models here.
-admin.site.register(Item)
 
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('item_name', 'item_price', 'image_preview')
+
+    def image_preview(self, obj):
+        if obj.item_image:
+            return format_html('<img src="{}" style="height:36px;" />', obj.item_image.url)
+        return "-"
+    image_preview.short_description = "Vorschau"
--- main_app/models.py
+++ main_app/models.py
@@ -2,21 +2,12 @@ from django.db import models
 
 
 class Item(models.Model):
-  
-  def __str__(self):
-    return self.item_name
-  
-  item_name = models.CharField(max_length=200)
-  item_desc = models.CharField()
-  item_price = models.IntegerField()
 
+    def __str__(self):
+        return self.item_name
 
-
-
-
-
-
-
-
-
-
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField()
+    item_price = models.IntegerField()
+    # item_image = models.CharField(max_length=500, default='https://cdn-icons-png.flaticon.com/512/1377/1377194.png')
+    item_image = models.ImageField(upload_to='items/', blank=True, null=True)
